2f1156a9632d387b3ec3a5e0e67379fc1dd86aeb
Ishwarya <ishwaryathendral@gmail.com>
2020-08-11 21:11:04 +0530

    [IMP] This repository consists of modules,classes and objects

---
+++ modules/index1.js
@@ -0,0 +1,29 @@
+//exported module user1 
+const user = require('./user1');
+
+//declared name and password
+const Name = "Ishu";
+const Password = "29112000" ;
+
+
+//two extra statements fot clarifications
+console.log("Username and Password is obtained succesfully");
+console.log("please wait until loading...");
+ 
+
+//conditions for checking the name and password
+if (Name == "Ishu" || Password == "29112000" )
+  {
+  console.log("Success !!!");
+  }
+
+else
+  {
+  console.log("Not matched,try again");
+  }
+
+
+
+
+
+
---
+++ modules/user1.js
@@ -0,0 +1,14 @@
+const getName = () => {
+  return 'Ishu';
+};
+
+const getPassword = () => {
+  return '29112000';
+};
+
+
+
+exports.getName = getName;
+exports.getPassword = getPassword;
+
+
