045802ba060fa8506cb0f42776e01a25ec35623d
Ishwarya <ishwaryathendral@gmail.com>
2020-08-23 11:57:07 +0530

    [IMP]

---
+++ objects.js
@@ -0,0 +1,5 @@
+console.log("Departments in cypher source");
+var student = { department:"machine learning", department1: "networking", department2: "gaming" };
+for (var item in student) {
+   console.log(student[item]);     
+}
--- objects/objects1.js
+++
@@ -1,8 +0,0 @@
-console.log("Departments In cypher source");
-let names = [
-   "Machine learning","Gaming","Networking"
-];
-
-for (let name of names) {
-  console.log(` ${name}`);
-}
