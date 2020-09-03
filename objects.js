cd46e22290ce3aade4c54a70a028107dbc619b83
Ishwarya <ishwaryathendral@gmail.com>
2020-09-03 17:43:09 +0530

    [IMP]

--- objects.js
+++ objects.js
@@ -1,5 +1,9 @@
+
 console.log("Departments in cypher source");
+//declaring object student
 var student = { department:"machine learning", department1: "networking", department2: "gaming" };
+
+//for in loop is used
 for (var item in student) {
    console.log(student[item]);     
 }
